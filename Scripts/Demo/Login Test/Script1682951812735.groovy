af2c37b327044b779569f4f73a131d3dd59cc722
adityasasmitapura <adityasasmitapura@localhost>
2023-05-07 23:14:14 +0700

    lagi

--- Scripts/Demo/Login Test/Script1682951812735.groovy
+++ Scripts/Demo/Login Test/Script1682951812735.groovy
@@ -27,5 +27,3 @@ WebUI.setEncryptedText(findTestObject('Object Repository/Page_OrangeHRM/input_Pa
 
 WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/button_Login'))
 
-WebUI.closeBrowser()
-
